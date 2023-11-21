4812311830de64c13bb45c43919a65c212cde560
Gresdian <chris_da98@outlook.it>
2023-11-21 10:06:59 +0100

    corrected price count

--- js/script.js
+++ js/script.js
@@ -5,18 +5,21 @@ let eta = prompt('inserisci la tua età');
 console.log(eta);
 
 let ticket = km *0.21;
-let n = ticket.toFixed(2)
+
 
 if(eta < 18){
-    ticket = ticket * 0.2;
+    sconto = ticket * 0.2;
+    ticket = ticket - sconto
     console.log(ticket,'paghi col 20% di sconto');
 }
 else if(eta > 65){
-    ticket = ticket * 0.4;
+    sconto =  ticket * 0.4;
+    ticket = ticket - sconto
     console.log(ticket,'paghi col 40% di sconto');
 }
 else {
     console.log(ticket, 'paghi tariffa intera');
 }
 
+let n = ticket.toFixed(2)
 document.getElementById("price").innerHTML = n;
